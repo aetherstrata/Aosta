ae76058931ad8f24ed973a7f100def94d7937654
Davide Pierotti <d.pierotti@live.it>
2022-10-30 18:34:39 +0100

    Add new icon codes

--- Animeikan.GUI/Models/FontAwesomeIcons.cs
+++ Animeikan.GUI/Models/FontAwesomeIcons.cs
@@ -3,13 +3,15 @@
 /// <summary>
 /// This class contains the unicode representation of all FontAwesome icons used throughout the app
 /// </summary>
-static class FontAwesomeIcons
+public static class FontAwesomeIcons
 {
-    public const string ArrowRight = "\u2192";
-    public const string Check = "\u2713";
-    public const string Gear = "\u2699";
-    public const string Film = "\uf008";
-    public const string User = "\uf007";
-    public const string Star = "\u2b50";
-    public const string Heart = "\u2665";
+  public const string ArrowRight = "\u2192";
+  public const string Check = "\u2713";
+  public const string Film = "\uf008";
+  public const string FloppyDisk = "\uf0c7";
+  public const string Gear = "\u2699";
+  public const string Heart = "\u2665";
+  public const string Plus = "\u002b";
+  public const string Star = "\u2b50";
+  public const string User = "\uf007";
 }
