41aad472c9cfde212108cee9f4e5b4bbdbe3fda5
Davide Pierotti <d.pierotti@live.it>
2023-03-22 02:13:06 +0100

    Add `IEnumerable` extension for fluent foreach

---
+++ Aosta.Core/Extensions/CollectionsExtensions.cs
@@ -0,0 +1,12 @@
+namespace Aosta.Core.Extensions;
+
+public static class CollectionsExtensions
+{
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (T item in source)
+        {
+            action(item);
+        }
+    }
+}
