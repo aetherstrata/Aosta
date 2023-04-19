8ace95b1d915c35cce3972f75fc7ca50ab90daf3
Davide Pierotti <d.pierotti@live.it>
2023-04-20 00:36:20 +0200

    Add `DaysOfWeek` enum to jikan wrapper

---
+++ Aosta.Jikan/Enums/DaysOfWeek.cs
@@ -0,0 +1,39 @@
+namespace Aosta.Jikan.Enums;
+
+public enum DaysOfWeek
+{
+    /// <summary>
+    /// Aired on mondays
+    /// </summary>
+    Mondays,
+
+    /// <summary>
+    /// Aired on tuesdays
+    /// </summary>
+    Tuesdays,
+
+    /// <summary>
+    /// Aired on wednesdays
+    /// </summary>
+    Wednesdays,
+
+    /// <summary>
+    /// Aired on thursdays
+    /// </summary>
+    Thursdays,
+
+    /// <summary>
+    /// Aired on fridays
+    /// </summary>
+    Fridays,
+
+    /// <summary>
+    /// Aired on saturdays
+    /// </summary>
+    Saturdays,
+
+    /// <summary>
+    /// Aired on sundays
+    /// </summary>
+    Sundays
+}
