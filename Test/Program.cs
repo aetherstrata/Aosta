bb397f36ab82dbde20f820faf221da3d141e91c6
Davide Pierotti <d.pierotti@live.it>
2024-01-11 15:17:50 +0100

    Remove old resources
    Will convert them to avares when needed

--- Test/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Aosta.Jikan;
-
-var jikan = new JikanConfiguration().Build();
-
-Console.WriteLine(jikan.GetAnimeAsync(1).Result.Data.Episodes);
