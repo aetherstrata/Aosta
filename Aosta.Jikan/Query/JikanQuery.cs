d45d162411e8f3034a38927a839d01c8b0f2a9b4
Davide Pierotti <d.pierotti@live.it>
2024-01-30 04:15:45 +0100

    Make `JikanQueryParameterSet` extend `Dictionary<>`

--- Aosta.Jikan/Query/JikanQuery.cs
+++ Aosta.Jikan/Query/JikanQuery.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 
-using Aosta.Common.Extensions;
-
 namespace Aosta.Jikan.Query;
 
-internal sealed class JikanQuery : IQuery, IReadOnlySet<IQueryParameter>
+internal sealed class JikanQuery : IQuery, IEnumerable
 {
     /// <summary>
     /// Query endpoint
@@ -71,7 +69,11 @@ internal sealed class JikanQuery : IQuery, IReadOnlySet<IQueryParameter>
     /// <returns>The updated query</returns>
     internal JikanQuery AddRange(JikanQueryParameterSet parameters)
     {
-        parameters.ForEach(_parameters.Add);
+        foreach (var param in parameters)
+        {
+            _parameters.TryAdd(param.Key, param.Value);
+        }
+
         return this;
     }
 
@@ -84,23 +86,8 @@ internal sealed class JikanQuery : IQuery, IReadOnlySet<IQueryParameter>
         return _endpoint + _parameters;
     }
 
-    public IEnumerator<IQueryParameter> GetEnumerator()
+    IEnumerator IEnumerable.GetEnumerator()
     {
         return _parameters.GetEnumerator();
     }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
-
-    public int Count => _parameters.Count;
-    public bool Contains(IQueryParameter item) => _parameters.Contains(item);
-
-    public bool IsProperSubsetOf(IEnumerable<IQueryParameter> other) => _parameters.IsProperSubsetOf(other);
-    public bool IsProperSupersetOf(IEnumerable<IQueryParameter> other) => _parameters.IsProperSupersetOf(other);
-    public bool IsSubsetOf(IEnumerable<IQueryParameter> other) => _parameters.IsSubsetOf(other);
-    public bool IsSupersetOf(IEnumerable<IQueryParameter> other) => _parameters.IsSupersetOf(other);
-    public bool Overlaps(IEnumerable<IQueryParameter> other) => _parameters.Overlaps(other);
-    public bool SetEquals(IEnumerable<IQueryParameter> other) => _parameters.SetEquals(other);
 }
--- Aosta.Jikan/Query/JikanQueryParameterSet.cs
+++ Aosta.Jikan/Query/JikanQueryParameterSet.cs
@@ -4,84 +4,74 @@ using Aosta.Jikan.Exceptions;
 
 namespace Aosta.Jikan.Query;
 
-public class JikanQueryParameterSet : IReadOnlySet<IQueryParameter>
+public class JikanQueryParameterSet : Dictionary<string, IQueryParameter>
 {
-    private readonly HashSet<IQueryParameter> _parameters = new(QueryParameter.NameComparer);
-
     internal JikanQueryParameterSet()
     {
     }
 
-    internal void Add(IQueryParameter parameter)
-    {
-        if (!_parameters.Add(parameter))
-        {
-            throw new JikanDuplicateParameterException($"A query parameter named {parameter.GetName()} already exists.", nameof(parameter));
-        }
-    }
-
     internal void Add<T>(string name, T value) where T : struct, Enum
     {
-        Add(new EnumQueryParameter<T>()
+        this[name] = new EnumQueryParameter<T>()
         {
             Name = name,
             Value = value
-        });
+        };
     }
 
     internal void Add(string name, double value)
     {
-        Add(new JikanQueryParameter<double>()
+        this[name] = new JikanQueryParameter<double>()
         {
             Name = name,
             Value = value
-        });
+        };
     }
 
 
     internal void Add(string name, long value)
     {
-        Add(new JikanQueryParameter<long>()
+        this[name] = new JikanQueryParameter<long>()
         {
             Name = name,
             Value = value
-        });
+        };
     }
 
     internal void Add(string name, int value)
     {
-        Add(new JikanQueryParameter<int>()
+        this[name] = new JikanQueryParameter<int>()
         {
             Name = name,
             Value = value
-        });
+        };
     }
 
     internal void Add(string name, string value)
     {
-        Add(new JikanQueryParameter<string>()
+        this[name] = new JikanQueryParameter<string>()
         {
             Name = name,
             Value = value
-        });
+        };
     }
 
     internal void Add(string name, char value) => Add(name, value.ToString());
 
     internal void Add(string name, bool value)
     {
-        Add(new BoolQueryParameter
+        this[name] = new BoolQueryParameter
         {
             Name = name,
             Value = value
-        });
+        };
     }
 
     internal void Add(string name) => Add(name, true);
 
     public override string ToString()
     {
-        string parameterString = string.Join("&", _parameters
+        string parameterString = string.Join("&", Values
             .Select(x => x.ToString())
             .Where(s => !string.IsNullOrEmpty(s)));
 
@@ -89,34 +79,4 @@ public class JikanQueryParameterSet : IReadOnlySet<IQueryParameter>
             ? string.Empty
             : "?" + parameterString;
     }
-
-    /// <inheritdoc />
-    public IEnumerator<IQueryParameter> GetEnumerator() => _parameters.GetEnumerator();
-
-    /// <inheritdoc />
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-    /// <inheritdoc />
-    public int Count => _parameters.Count;
-
-    /// <inheritdoc />
-    public bool Contains(IQueryParameter item) => _parameters.Contains(item);
-
-    /// <inheritdoc />
-    public bool IsProperSubsetOf(IEnumerable<IQueryParameter> other) => _parameters.IsProperSubsetOf(other);
-
-    /// <inheritdoc />
-    public bool IsProperSupersetOf(IEnumerable<IQueryParameter> other) => _parameters.IsProperSupersetOf(other);
-
-    /// <inheritdoc />
-    public bool IsSubsetOf(IEnumerable<IQueryParameter> other) => _parameters.IsSubsetOf(other);
-
-    /// <inheritdoc />
-    public bool IsSupersetOf(IEnumerable<IQueryParameter> other) => _parameters.IsSupersetOf(other);
-
-    /// <inheritdoc />
-    public bool Overlaps(IEnumerable<IQueryParameter> other) => _parameters.Overlaps(other);
-
-    /// <inheritdoc />
-    public bool SetEquals(IEnumerable<IQueryParameter> other) => _parameters.SetEquals(other);
 }
