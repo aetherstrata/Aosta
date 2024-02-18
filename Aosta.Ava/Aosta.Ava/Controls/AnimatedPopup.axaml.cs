019f416a6c7def57c15fda70a3e9cbb5672a6aa0
Davide Pierotti <d.pierotti@live.it>
2024-02-18 18:36:42 +0100

    Add custom animated popup control

---
+++ Aosta.Ava/Aosta.Ava/Controls/AnimatedPopup.axaml.cs
@@ -0,0 +1,217 @@
+// Copyright (c) Davide Pierotti <d.pierotti@live.it>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Threading;
+
+using Aosta.Ava.Extensions;
+
+using Avalonia;
+using Avalonia.Animation.Easings;
+using Avalonia.Controls;
+using Avalonia.Layout;
+using Avalonia.Media;
+using Avalonia.Styling;
+using Avalonia.Threading;
+
+using Splat;
+
+using ILogger = Serilog.ILogger;
+
+namespace Aosta.Ava.Controls;
+
+// ReSharper disable InconsistentNaming
+public class AnimatedPopup : ContentControl
+{
+    public AnimatedPopup()
+    {
+        _origOpacity = Opacity;
+
+        Opacity = 0;
+
+        _sizingTimer = new Timer(state =>
+        {
+            if (_foundSizing) return;
+
+            _foundSizing = true;
+
+            Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                _desiredSize = DesiredSize - Margin;
+
+                updateAnimation();
+            });
+        });
+
+        _animationTimer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(1.0 / 60),
+        };
+
+        _animationTimer.Tick += animationTick;
+
+        _underlay = new Border
+        {
+            IsVisible = false,
+            VerticalAlignment = VerticalAlignment.Stretch,
+            HorizontalAlignment = HorizontalAlignment.Stretch,
+            Background = Brushes.Transparent,
+            CornerRadius = new CornerRadius(0),
+            BorderThickness = new Thickness(0),
+            ZIndex = 10,
+        };
+
+        _underlay.Tapped += (_, _) => Close();
+    }
+
+    #region Private Members
+
+    private readonly ILogger _log = Locator.Current.GetSafely<ILogger>();
+
+    private readonly SineEaseInOut _easing = new();
+    private readonly DispatcherTimer _animationTimer;
+    private readonly double _origOpacity;
+    private readonly Timer _sizingTimer;
+    private readonly Control _underlay;
+
+
+    private int _currentTick;
+    private Size _desiredSize;
+    private bool _firstAnimation = true;
+    private bool _foundSizing;
+    private int _totalTicks;
+
+    #endregion
+
+    #region Control Properties
+
+    private TimeSpan _animationTime = TimeSpan.FromSeconds(0.5);
+
+    public static readonly DirectProperty<AnimatedPopup, TimeSpan> AnimationTimeProperty =
+        AvaloniaProperty.RegisterDirect<AnimatedPopup, TimeSpan>(
+            nameof(AnimationTime), o => o.AnimationTime, (o, v) => o.AnimationTime = v);
+
+    public TimeSpan AnimationTime
+    {
+        get => _animationTime;
+        set => SetAndRaise(AnimationTimeProperty, ref _animationTime, value);
+    }
+
+    private bool _isOpen;
+
+    public static readonly DirectProperty<AnimatedPopup, bool> IsOpenProperty =
+        AvaloniaProperty.RegisterDirect<AnimatedPopup, bool>(
+            nameof(IsOpen), o => o.IsOpen, (o, v) => o.IsOpen = v);
+
+    public bool IsOpen
+    {
+        get => _isOpen;
+        set => SetAndRaise(IsOpenProperty, ref _isOpen, value);
+    }
+
+    #endregion
+
+    public void Open()
+    {
+        IsOpen = true;
+
+        updateAnimation();
+    }
+
+    public void Close()
+    {
+        IsOpen = false;
+
+        updateAnimation();
+    }
+
+    public override void Render(DrawingContext context)
+    {
+        if (!_foundSizing)
+        {
+            _sizingTimer.Change(100, int.MaxValue);
+        }
+
+        base.Render(context);
+    }
+
+    private void animationTick(object? sender, EventArgs args)
+    {
+        if (_firstAnimation)
+        {
+            _firstAnimation = false;
+
+            completeAnimation();
+
+            Opacity = _origOpacity;
+
+            return;
+        }
+
+        if (_isOpen && _currentTick > _totalTicks ||
+            !_isOpen && _currentTick == 0)
+        {
+            _animationTimer.Stop();
+
+            completeAnimation();
+
+            return;
+        }
+
+        _currentTick += _isOpen ? 1 : -1;
+
+        _log.Verbose("Animation tick: {Tick}", _currentTick);
+
+        double progress = (double)_currentTick / _totalTicks;
+
+        Width = _desiredSize.Width * _easing.Ease(progress);
+        Height = _desiredSize.Height * _easing.Ease(progress);
+    }
+
+    private void updateAnimation()
+    {
+        if (!_foundSizing) return;
+
+        _totalTicks = (int)(_animationTime / _animationTimer.Interval);
+        _animationTimer.Start();
+    }
+
+    private void completeAnimation()
+    {
+        if (_isOpen)
+        {
+            Width = _desiredSize.Width;
+            Height = _desiredSize.Height;
+
+            if (Parent is Grid g)
+            {
+                _underlay.IsVisible = true;
+
+                if (g.RowDefinitions.Count > 0)
+                {
+                    _underlay.SetValue(Grid.RowSpanProperty, g.RowDefinitions.Count);
+                }
+
+                if (g.ColumnDefinitions.Count > 0)
+                {
+                    _underlay.SetValue(Grid.ColumnSpanProperty, g.ColumnDefinitions.Count);
+                }
+
+                g.Children.Insert(0, _underlay);
+            }
+        }
+        else
+        {
+            Width = 0;
+            Height = 0;
+
+            if (Parent is Grid g)
+            {
+                if (g.Children.Contains(_underlay))
+                {
+                    g.Children.Remove(_underlay);
+                }
+            }
+        }
+    }
+}
